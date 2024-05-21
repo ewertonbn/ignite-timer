9af37c06ed41fe046265f750bebf908e8d462968
ewertonbn <ewertonbn.dev@gmail.com>
2024-05-21 14:18:23 -0300

    feat: reducing countdown and interrupt cycle

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -57,6 +57,7 @@ export const TaskInput = styled(BaseInput)`
 
 export const MinutesAmountInput = styled(BaseInput)`
   width: 4rem;
+  text-align: center;
 `
 
 export const CountdownContainer = styled.div`
@@ -82,7 +83,7 @@ export const Separator = styled.div`
   display: flex;
   justify-content: center;
 `
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -91,18 +92,29 @@ export const StartCountdownButton = styled.button`
   align-items: center;
   justify-content: center;
   gap: 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
   font-weight: bold;
   cursor: pointer;
-  background: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme['gray-100']};
   transition: all 0.2s;
-
-  &:not(:disabled):hover {
-    background: ${(props) => props.theme['green-700']};
-  }
 
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
+  }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
   }
 `
